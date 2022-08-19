96807a74946beb4b61235c420f296b1e7175c5b8
Daniel Dimitrov <daniel@compojoom.com>
2022-08-19 14:02:58 +0300

    Enforce dark mode for now

--- packages/website/pages/_document.tsx
+++ packages/website/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html className={"dark"}>
       <Head />
       <body className={'bg-white dark:bg-zinc-800'}>
         <Main />
