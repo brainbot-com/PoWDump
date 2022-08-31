1c9e95d48d9eb0f76c340e365a3b57b0c8a91883
Daniel Dimitrov <daniel@compojoom.com>
2022-08-31 10:37:09 +0300

    Make the contract deployments configurable
    We are planning to have no fee on PoW and a fee on PoS. We cannot

--- packages/contracts/deploy/001_Swap.ts
+++ packages/contracts/deploy/001_Swap.ts
@@ -1,7 +1,15 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatUserConfig} from "hardhat/config";
+import {deploymentsConf} from "../hardhat.config";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+
+  const {network} = hre.hardhatArguments
+  const userConfig: HardhatUserConfig & {deployments?: deploymentsConf} = hre.userConfig
+  const deploymentConfig = userConfig.deployments
+
+
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
   const {deployer} = await getNamedAccounts();
@@ -9,7 +17,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // TODO make configurable
   const recipientChangeLockDuration = 10*60;
   const feeRecipient = "0x0000000000000000000000000000000000001234";
-  const feePerMillion = 1000
+  const feePerMillion = network ? deploymentConfig?.[network]?.defaultFeePerMillion || 0 : 0;
 
   await deploy('EtherSwap', {
     from: deployer,
