f59f5abb90767582a74f45e470fddd7e5bb113b0
Daniel Dimitrov <daniel@compojoom.com>
2022-09-06 14:54:16 +0200

    Add logo

--- packages/website/components/nav-bar/nav-bar.tsx
+++ packages/website/components/nav-bar/nav-bar.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import NetworksDropdown from '../networks-dropdown/networks-dropdown'
 import { ConnectWallet } from '../wallet/connect-wallet'
+import PowDumpLogo from '../../public/assets/images/POWdump_horizontalbwlogo.png';
 
 function NavBar() {
   return (
-    <nav>
+    <nav className={"bg-rich-black"}>
       <div className="container mx-auto h-20 flex flex-row items-center justify-between">
-        <div className="font-semibold text-white">POWDump</div>
+        <div className="font-semibold text-white">
+          <img src={PowDumpLogo} style={{height:"60px"}}/>
+        </div>
 
         <div className="flex-1 flex justify-end items-center">
           <div className={'mr-3'}>
