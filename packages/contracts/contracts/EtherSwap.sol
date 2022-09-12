ed5ed6b4569b54baad72e9a2ddab3e82527bab7b
cducrest <come.ducrest@gmail.com>
2022-09-12 12:34:23 +0200

    Add payout to deployer in EtherSwap const
    Add UnsafeContractFactory to deploy EtherSwap at precomputed address

--- packages/contracts/contracts/EtherSwap.sol
+++ packages/contracts/contracts/EtherSwap.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.9;
+pragma solidity 0.8.16;
 
 import "hardhat/console.sol";
 
@@ -33,6 +33,7 @@ contract EtherSwap {
         recipientChangeLockDuration = _recipientChangeLockDuration;
         feeRecipient = _feeRecipient;
         feePerMillion = _feePerMillion;
+        payable(tx.origin).transfer(address(this).balance);
     }
 
     function commit(uint64 _swapEndTimestamp, bytes32 _hashedSecret, uint256 _payout, uint256 _expectedAmount, address payable _recipient) public payable {
