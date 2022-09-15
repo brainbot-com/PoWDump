a50c9717d4c0b73a7f51e9aa5adc6be61600423b
Daniel Dimitrov <daniel@compojoom.com>
2022-09-15 23:56:34 +0200

    Fix - T&C doesn’t show & hide max button
    For now hide the max button when the user has less tahn 0.03 eth. There must be a better way to calculate how much one should leave in the account.
    
    For T&C we basically use a hack. In the exported version every file has .html extension. If we don’t use this when we open the page in new tab it’s not being found. Nextjs deals with pages by loading them with js, but we would like to display the terms and conditions on a new page so the user won’t lose the filled out form. (we could do a modal at one point)

--- packages/website/components/dump-box/dump-form.tsx
+++ packages/website/components/dump-box/dump-form.tsx
@@ -290,7 +290,8 @@ export function DumpForm() {
             {isSwapEnabled ?
                 <div className={'flex flex-row justify-end items-center mr-5 text-gray text-sm mb-2'}>
                   Balance: {currencyAmount.toFixed(5)}
-                  {maxAmountFormatted === ethPoWAmount ? null : (
+                  {/*{ hide the button for now - there must be a better way to take .03 from the current user amount}*/}
+                  {BigNumber.from(parseEther(maxAmountFormatted)).lt(parseEther("0.03")) || maxAmountFormatted === ethPoWAmount ? null : (
                       <button
                           disabled={!isSwapEnabled}
                           className={
@@ -356,8 +357,9 @@ export function DumpForm() {
           />{' '}
           <span className={!isSwapEnabled ? 'text-gray' : ''}>
             I understand and accept the{' '}
-            <Link href={'/terms_and_conditions'} target={'_blank'} rel="noreferrer" className={'underline'}>
-              Terms & Conditions
+            {/*{the link doesn't work when in dev mode, but works when the nextjs page is exported}*/}
+            <Link href={'/terms_and_conditions.html'}  passHref>
+              <a className={'underline'} target={'_blank'} rel="noreferrer">Terms & Conditions</a>
             </Link>
           </span>
         </label>
