ec4904251614551c3e9c29fd91e9cc841ab5c5ec
Daniel Dimitrov <daniel@compojoom.com>
2022-09-16 13:03:00 +0200

    feat: Add a warning if a user tries to close page
    If the user tries to leave, but he has an active swap the app the browser will display a warning. Sadly in newer browsers it is no longer possible to set message in the browser.

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -2,13 +2,32 @@ import type {NextPage} from 'next'
 
 import Layout from "../components/layout";
 import dynamic from "next/dynamic";
+import {useEffect} from "react";
+import {useStore} from "../store";
 
 const DumpBox = dynamic<{}>(() => import('../components/dump-box').then(mod => mod.DumpBox), {
     ssr: false,
 })
 
-
 const Home: NextPage = () => {
+    const processingCommitment = useStore(state => state.processingCommitment)
+
+    useEffect(() => {
+        const beforeUnload = (e: BeforeUnloadEvent) => {
+            if (processingCommitment) {
+                e.preventDefault()
+
+                // return value is useless on modern browsers https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#browser_compatibility
+                return e.returnValue = 'It looks like you have some non-complete swaps. Leaving the page would prevent you from completing them. Are you sure you want to leave?'
+            }
+        }
+
+        window.addEventListener('beforeunload', beforeUnload)
+
+        return () => {
+            window.removeEventListener('keydown', beforeUnload);
+        };
+    }, [processingCommitment])
 
     return (
         <Layout title={'PoWDump - A PoW ETH dump DApp using cross-chain atomic swaps.'}>
