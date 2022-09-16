881988ccc78269821cdf15745ba611e5860715d1
Daniel Dimitrov <daniel@compojoom.com>
2022-09-16 16:49:39 +0200

    chore: Add legal disclaimer on price feed

--- packages/website/components/dump-box/suggested-price-tooltip-help.tsx
+++ packages/website/components/dump-box/suggested-price-tooltip-help.tsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { Tooltip } from '../tooltip'
 import config from '../../config'
 
-export function SuggestedPriceTooltipHelp(props: { price: string; children: React.ReactNode, strategy: 'absolute' | 'fixed' }) {
+export function SuggestedPriceTooltipHelp(props: {
+  price: string
+  children: React.ReactNode
+  strategy: 'absolute' | 'fixed'
+}) {
   return (
     <Tooltip
       label={
-        <div className="flex items-center bg-rich-black-lighter text-white text-sm font-medium p-2 rounded-sm shadow-md max-w-[250px] border border-rich-black-lightest">
-          This price is the latest Coingecko price {props.price} minus {config.DUMP_DISCOUNT}%. For a volatile market,
-          the discount improves the likelihood for the transaction to go through.
+        <div className="flex flex-col items-center bg-rich-black-lighter text-white text-xs font-medium p-2 rounded-sm shadow-md max-w-[250px] border border-rich-black-lightest">
+          <p>
+            This price is the latest Coingecko price {props.price} minus {config.DUMP_DISCOUNT}%. For a volatile market,
+            the discount improves the likelihood for the transaction to go through.
+          </p>
+          <p className={"mt-2"}>
+            Third-party and user-generated content is made available for informational purposes only and should not be
+            treated as investment advice. POWdump does not endorse or recommend such content and is not responsible for
+            its accuracy.
+          </p>
         </div>
       }
       placement={'top'}
