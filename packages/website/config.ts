8639f39cd5c150838211e88c73eaef9d41754005
Daniel Dimitrov <daniel@compojoom.com>
2022-09-16 08:05:57 +0200

    Crash the app if missing a required .env variable
    We can support dumping from multiple chains and we provide the config for this in the .env variable.
    
    This change makes it easier to detect misconfigurations.

--- packages/website/config.ts
+++ packages/website/config.ts
@@ -4,12 +4,42 @@ const subgraphPosUrl = process.env.NEXT_PUBLIC_SUBGRAPH_POS_URL || ''
 const posContractAddress = process.env.NEXT_PUBLIC_ETH_POS_CONTRACT_ADDRESS || ''
 const priceFeedUrls = process.env.NEXT_PUBLIC_PRICE_FEED_API_URLS || ''
 const priceFeedCurrencyIds = process.env.NEXT_PUBLIC_PRICE_CURRENCY_IDS || ''
+const enforceSwapsOnChains = process.env.NEXT_PUBLIC_ENFORCE_SWAP_ON_CHAINS
+    ? process.env.NEXT_PUBLIC_ENFORCE_SWAP_ON_CHAINS.split(',').map(Number)
+    : []
 
-if (contractAddresses.length === 0) {
+const getObjectOutOfConfigString = (configString: string): { [key: string]: string } => {
+    return configString
+        .split(',')
+        .map(key_item => key_item.split('|'))
+        .reduce((prev: { [chainId: string]: string }, curr) => {
+            prev[curr[0]] = curr[1]
+            return prev
+        }, {})
+}
+const isMissingValuesForAllPoWChains = (configString: string, envName: string): boolean => {
+    const configObject = getObjectOutOfConfigString(configString)
+
+    for (const chainId of enforceSwapsOnChains) {
+        if (!configString.includes(chainId.toString())) {
+                if(!configObject[chainId]) {
+                    throw new Error(`Missing config for chainId ${chainId} in ${envName}`)
+                }
+        }
+    }
+
+    return false
+}
+
+if(enforceSwapsOnChains.length === 0) {
+    throw new Error('NEXT_PUBLIC_ENFORCE_SWAP_ON_CHAINS must be a defined environment variable')
+}
+
+if (contractAddresses.length === 0 || isMissingValuesForAllPoWChains(contractAddresses, 'NEXT_PUBLIC_ETH_POW_CONTRACT_ADDRESSES')) {
   throw new Error('NEXT_PUBLIC_ETH_POW_CONTRACT_ADDRESSES must be a defined environment variable')
 }
 
-if (subgraphPowUrls.length === 0) {
+if (subgraphPowUrls.length === 0 || isMissingValuesForAllPoWChains(subgraphPowUrls, 'NEXT_PUBLIC_SUBGRAPH_POW_URLS')) {
   throw new Error('NEXT_PUBLIC_SUBGRAPH_POW_URLS must be a defined environment variable')
 }
 
@@ -21,23 +51,15 @@ if (posContractAddress.length === 0) {
   throw new Error('NEXT_PUBLIC_ETH_POS_CONTRACT_ADDRESS must be a defined environment variable')
 }
 
-if (priceFeedUrls.length === 0) {
+if (priceFeedUrls.length === 0 || isMissingValuesForAllPoWChains(priceFeedUrls, 'NEXT_PUBLIC_PRICE_FEED_API_URLS')) {
   throw new Error('NEXT_PUBLIC_PRICE_FEED_API_URLS must be a defined environment variable')
 }
 
-if (priceFeedCurrencyIds.length === 0) {
+if (priceFeedCurrencyIds.length === 0 || isMissingValuesForAllPoWChains(priceFeedCurrencyIds, 'NEXT_PUBLIC_PRICE_CURRENCY_IDS')) {
   throw new Error('NEXT_PUBLIC_PRICE_CURRENCY_ID must be a defined environment variable')
 }
 
-const getObjectOutOfConfigString = (configString: string): { [key: string]: string } => {
-  return configString
-    .split(',')
-    .map(key_item => key_item.split('|'))
-    .reduce((prev: { [chainId: string]: string }, curr) => {
-      prev[curr[0]] = curr[1]
-      return prev
-    }, {})
-}
+
 export const config: {
   ETH_POS_CONTRACT_ADDRESS: string
   ETH_POW_CONTRACT_ADDRESSES: { [chainId: string]: string }
@@ -63,9 +85,7 @@ export const config: {
     : 0,
   PRICE_FEED_API_URLS: getObjectOutOfConfigString(priceFeedUrls),
   PRICE_CURRENCY_IDS: getObjectOutOfConfigString(priceFeedCurrencyIds),
-  ENFORCE_SWAP_ON_CHAINS: process.env.NEXT_PUBLIC_ENFORCE_SWAP_ON_CHAINS
-    ? process.env.NEXT_PUBLIC_ENFORCE_SWAP_ON_CHAINS.split(',').map(Number)
-    : [],
+  ENFORCE_SWAP_ON_CHAINS: enforceSwapsOnChains,
   POS_CHAIN_ID: process.env.NEXT_PUBLIC_POS_CHAIN_ID ? Number(process.env.NEXT_PUBLIC_POS_CHAIN_ID) : 1,
   NETWORK_IDS_FOR_DROPDOWN: process.env.NEXT_PUBLIC_NETWORK_IDS_FOR_DROPDOWN
     ? process.env.NEXT_PUBLIC_NETWORK_IDS_FOR_DROPDOWN.split(',').map(Number)
